29446606ff7b64aff4e55472a21252537e8e986d
codyveladev <44760218+codyveladev@users.noreply.github.com>
2020-10-17 01:21:12 -0500

    spelling errors

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function about() {
   return (
     <div className="text-center">
-      <h1 className="diplay-4 text-danger">About Contact Managment System</h1>
+      <h1 className="diplay-4 text-danger">About Contact Management System</h1>
       <p className="lead">
         This a demo application created by Cody Vela to learn more about
         development using React.js.
